5a5825fee2b52f8b8eead5f81888271c4a405abd
yamachu <yamachu.dev@gmail.com>
2026-02-15 16:22:00 +0900

    middlewareを突っ込む方法の検証

--- n8n-management-console/app/server.ts
+++ n8n-management-console/app/server.ts
@@ -1,7 +1,16 @@
+import { Hono, type Env } from "hono";
 import { showRoutes } from "hono/dev";
 import { createApp } from "honox/server";
 
-const app = createApp();
+const composeMiddlewareApp = new Hono<Env>();
+composeMiddlewareApp.use("*", async (c, next) => {
+  console.log(`Received request: ${c.req.method} ${c.req.url}`);
+  await next();
+});
+
+const app = createApp<Env>({
+  app: composeMiddlewareApp,
+});
 
 showRoutes(app);
 
