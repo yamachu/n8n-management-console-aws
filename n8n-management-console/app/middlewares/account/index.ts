a7a0e68f569acc5a35e249a15d7357d2ffc10836
yamachu <yamachu.dev@gmail.com>
2026-02-18 01:54:02 +0900

    createMiddlewareを使うほうが記述量は減るかも

--- n8n-management-console/app/middlewares/account/index.ts
+++ n8n-management-console/app/middlewares/account/index.ts
@@ -1,16 +1,15 @@
-import type { Env, MiddlewareHandler } from "hono";
+import type { Env } from "hono";
+import { createMiddleware } from "hono/factory";
 
 import { ForbiddenError } from "../../exceptions";
 import type { UserRepository } from "../../infrastructures/userRepository";
 
-export const requireAccount: (
-  userRepository: UserRepository,
-  email: string,
-) => MiddlewareHandler<Env> = (userRepository, email) => async (c, next) => {
-  const currentUser = await userRepository.fetchUserByEmail(email);
-  if (!currentUser) {
-    throw new ForbiddenError("ログインユーザが見つかりません");
-  }
-  c.set("CURRENT_USER", currentUser);
-  await next();
-};
+export const requireAccount = (userRepository: UserRepository, email: string) =>
+  createMiddleware<Env>(async (c, next) => {
+    const currentUser = await userRepository.fetchUserByEmail(email);
+    if (!currentUser) {
+      throw new ForbiddenError("ログインユーザが見つかりません");
+    }
+    c.set("CURRENT_USER", currentUser);
+    await next();
+  });
--- n8n-management-console/app/middlewares/auth/cognito/index.ts
+++ n8n-management-console/app/middlewares/auth/cognito/index.ts
@@ -1,31 +1,36 @@
-import type { Context, Env, MiddlewareHandler } from "hono";
+import type { Context, Env } from "hono";
+import { createMiddleware } from "hono/factory";
 
 import type { AuthClient } from "..";
 import { UnauthorizedError } from "../../../exceptions";
 import { createAuthClient } from "./client";
 
-export const requireAuth: (
+export const requireAuth = (
   authClient?: AuthClient,
   handlerFilter?: (c: Context) => boolean,
-) => MiddlewareHandler<Env> = (authClient, handlerFilter) => async (c, next) => {
-  if (handlerFilter) {
-    const shouldAuthenticate = handlerFilter(c);
-    if (!shouldAuthenticate) {
-      await next();
-      return;
+) =>
+  createMiddleware<Env>(async (c, next) => {
+    if (handlerFilter) {
+      const shouldAuthenticate = handlerFilter(c);
+      if (!shouldAuthenticate) {
+        await next();
+        return;
+      }
     }
-  }
-  const client = authClient || (await createAuthClient({}));
+    const client = authClient || (await createAuthClient({}));
 
-  try {
-    const accessToken = await client.extractToken(c.req.header());
-    const user = await client.fetchUser(accessToken);
-    c.set("USER_EMAIL", user.email);
-  } catch (error) {
-    throw new UnauthorizedError("トークンの検証に失敗しました。再度ログインしてください。", {
-      cause: error,
-    });
-  }
+    try {
+      const accessToken = await client.extractToken(c.req.header());
+      const user = await client.fetchUser(accessToken);
+      c.set("USER_EMAIL", user.email);
+    } catch (error) {
+      throw new UnauthorizedError(
+        "トークンの検証に失敗しました。再度ログインしてください。",
+        {
+          cause: error,
+        },
+      );
+    }
 
-  await next();
-};
+    await next();
+  });
