83d19d05eb2e3054473ef26ddc5b2692e703daf3
yamachu <yamachu.dev@gmail.com>
2026-02-19 01:05:40 +0900

    .localを見るための作りにしておく

--- n8n-management-console/vite.config.ts
+++ n8n-management-console/vite.config.ts
@@ -4,14 +4,14 @@ import tailwindcss from "@tailwindcss/vite";
 import honox from "honox/vite";
 import { defineConfig } from "vite";
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [
     honox({
       devServer: { adapter },
       client: { input: ["/app/client.ts", "/app/style.css"] },
-      entry: "/app/server.tsx", // TODO: localでの開発は /app/server.local.tsx に向けるようにする
+      entry: `/app/server${command === "serve" ? ".local" : ""}.tsx`,
     }),
     tailwindcss(),
     build(),
   ],
-});
+}));
