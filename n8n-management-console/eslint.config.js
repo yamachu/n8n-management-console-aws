6a976b9633836419871ac53ff7dfd16bb9247935
yamachu <yamachu.dev@gmail.com>
2026-02-22 12:50:02 +0900

    SafeQL使えるようにする

---
+++ n8n-management-console/eslint.config.js
@@ -0,0 +1,34 @@
+import safeql from "@ts-safeql/eslint-plugin/config";
+import { defineConfig, globalIgnores } from "eslint/config";
+import tseslint from "typescript-eslint";
+
+import { enableSafeQL, safeQLDatabaseUrl } from "./.env.eslint.mjs";
+
+export default defineConfig(
+  globalIgnores(["dist/**", "*.js", "*.mjs"]),
+  {
+    files: [
+      "vite.config.ts" /** enableSafeQL===falseの時に対象がなくなってしまうため */,
+    ],
+  },
+  {
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  ...(enableSafeQL
+    ? [
+        {
+          files: ["**/infrastructures/**/*.ts"],
+          ...safeql.configs.connections({
+            databaseUrl: safeQLDatabaseUrl,
+            targets: [{ tag: "sql", transform: "{type}[]" }],
+          }),
+        },
+      ]
+    : []),
+);
